7984983d51d9c7da80dc62dce14430c923723527
Ann Blodgett <w.annblodgett@gmail.com>
2016-04-16 11:00:15 -0600

    LPTHW Exercise 1 A Good First Program
     LPTHW Exercise 1 A Good First Program http://learnpythonthehardway.org/book/ex1.html
     Completed 4.16.16 
    Write a first program in the python language.  Learn to use 'print', and how to save a file using .py extension.
    Learn to run python program using the PowerShell terminal. Familiarize with notepad++ interface.

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# LPTHW Exercise 1 A Good First Program http://learnpythonthehardway.org/book/ex1.html
+# Completed 4.16.16 
+
+#Write a first program in the python language.  Learn to use 'print', and how to save a file using .py extension.
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
